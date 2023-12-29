7868d740f6a3459155439fcf4cb6aa81f9c55f10
DTIGitOne <dammirtaljanovic@gmail.com>
2023-12-29 16:18:58 +0100

    vjezna1

--- js/extractor.js
+++ js/extractor.js
@@ -4,26 +4,39 @@ let right = document.getElementById("right");
 let root = document.getElementById("root");
 
 let regexUrl = /(https?\:\/\/)?(www\.)?\w+\.\w+/gi;
+let regexHttp = /^https\:\/\//gi;
 
 extract.addEventListener('click' , function() {
-
+  
    let link = textareaId.value.match(regexUrl);
    
    if (link.length) {
-      
+   
       for (i=0 ; i < link.length ; i++) {
+
          let lickClick = document.createElement('div');
          lickClick.className = "linkClick";
          right.append(lickClick);
 
          let linkCreate = document.createElement("a");
          linkCreate.innerHTML = link[i];
-         lickClick.append(linkCreate);
-         linkCreate.href = link[i];
 
          let buttonDelete = document.createElement("button");
          buttonDelete.className = "buttonDelete";
+
+         let lt = link[i];
+
+         if (regexHttp.test(lt)) {
+            lt = "https://" - link[i];
+         }
+
+         if (!regexHttp.test(lt)) {
+            lt = "https://" + link[i];
+         }
          
+         linkCreate.href = lt;
+         lickClick.append(linkCreate);
+
          buttonDelete.addEventListener('click' , function() {
             buttonDelete.remove();
             linkCreate.remove();
@@ -33,5 +46,6 @@ extract.addEventListener('click' , function() {
          lickClick.append(buttonDelete);
       }
    }
+  textareaId.innerHTML = "";
 }
 );
